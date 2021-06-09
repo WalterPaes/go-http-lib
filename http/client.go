cdfaecc9c9f61751ff83d8858de2f32a7753ef17
Walter Monteiro <picpay@PPM-SPO-15607.local>
2021-06-09 17:13:01 -0300

    Create http client lib

---
+++ http/client.go
@@ -0,0 +1,85 @@
+package http
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type Client struct {
+	url          string
+	headers      map[string]string
+	client       *http.Client
+	request      *http.Request
+	responseBody io.ReadCloser
+	StatusCode   int
+}
+
+func New(url string) *Client {
+	return &Client{
+		url:    url,
+		client: &http.Client{},
+	}
+}
+
+func (c *Client) AddHeader(key, value string) *Client {
+	c.headers[key] = value
+	return c
+}
+
+func (c *Client) SetTimeout(timeout int64) *Client {
+	c.client.Timeout = time.Duration(timeout)
+	return c
+}
+
+func (c *Client) Post(path string, body interface{}) (*Client, error) {
+	data, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := http.NewRequest(http.MethodPost, c.url+path, bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+
+	c.request = request
+	c.setHeaders()
+	return c.execute()
+}
+
+func (c *Client) setHeaders() {
+	for key, value := range c.headers {
+		c.request.Header.Set(key, value)
+	}
+}
+
+func (c *Client) execute() (*Client, error) {
+	response, err := c.client.Do(c.request)
+	if err != nil {
+		return nil, err
+	}
+
+	c.StatusCode = response.StatusCode
+	c.responseBody = response.Body
+	return c, nil
+}
+
+func (c *Client) Decode(i interface{}) (interface{}, error) {
+	defer c.responseBody.Close()
+
+	body, err := ioutil.ReadAll(c.responseBody)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(body, &i)
+	if err != nil {
+		return nil, err
+	}
+
+	return i, nil
+}
